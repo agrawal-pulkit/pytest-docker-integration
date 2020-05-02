d60923dfaa7cee5e1bb3984f4f8704e901bba207
Pulkit Agrawal <agrawal.pulkit1994@gmail.com>
2020-05-02 21:59:33 +0000

    adding mysql db setup with docker-compose for testcases

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 import time
-
+import MySQLdb
 import redis
 from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
+def db_connection():
+    try:
+        db_conn = MySQLdb.connect(host='mysql',user='ipa',passwd='password',db='ipa')
+        cursor = db_conn.cursor()
+        return db_conn,cursor
+    except MySQLdb.Error as e:
+        print("Exception in db connection: ", e)
 
 def get_hit_count():
     retries = 5
@@ -25,3 +32,12 @@ def add(a, b):
 def hello():
     count = get_hit_count()
     return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route('/mysql')
+def hello_mysql():
+    _, cursor = db_connection()
+    print("cursor: ", cursor)
+    sql = "show tables"
+    cursor.execute(sql)
+    fetch_data = cursor.fetchall()
+    return 'TABELS : {}\n'.format(fetch_data)
--- tests/test_app.py
+++ tests/test_app.py
@@ -21,3 +21,13 @@ def test_api(count):
         print("exception: ", e)
         assert False
     
+def test_mysql():
+    print("testing mysql")
+    try:
+        time.sleep(1)
+        r = requests.get('http://0.0.0.0:5000/mysql')
+        print(r.text)
+        assert True
+    except Exception as e:
+        print("exception: ", e)
+        assert False
